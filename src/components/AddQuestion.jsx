3031a8fa66776adff232cb88a5878e07680dcf83
Kirushanth <kirushan06@gmail.com>
2024-07-14 10:55:09 +0530

    First commit

--- src/components/AddQuestion.jsx
+++ src/components/AddQuestion.jsx
@@ -186,6 +186,7 @@ export default function SimpleContainer() {
               Add New
             </MenuItem>
           </TextField>
+          
             <br/>
           {isNewSubjectOpen && (
             <TextField
